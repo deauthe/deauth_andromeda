f25d4e7564790df17fdb53335bbf3cbccd646f7f
Abhshek Kumar <abui8994ty@gmail.com>
2024-04-08 19:51:31 +0530

    added nav add to cart component

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -5,6 +5,7 @@ import { IoMdHeartEmpty } from "react-icons/io";
 import { BsCart } from "react-icons/bs";
 import { ConnectWallet } from "@/modules/wallet";
 import Image from "next/image";
+import { Dropdown } from "../ModalWindow/dropdown";
 interface Category {
   id: number;
   attributes: {
@@ -51,17 +52,13 @@ const Navbar = () => {
             </div>
           </div>
 
-          <Link href="/cart">
+          {/* <Link href="/cart">
             <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
               <BsCart className={`text-[15px] text-white md:text-[20px]`} />
-              {/* TODO */}
-              {/* {cartItems.length > 0 && (
-								<div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
-									{cartItems.length}
-								</div>
-							)} */}
+              
             </div>
-          </Link>
+          </Link> */}
+          <Dropdown/>
         </div>
 
         {/* profile  dropdown section  */}
